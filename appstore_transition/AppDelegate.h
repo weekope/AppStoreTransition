70ae1853b1eb6f122f4786d080028fdf39f32b56
周希 <zhouxi653@pingan.com.cn>
2018-01-15 10:55:45 +0800

    Initial Commit

---
+++ appstore_transition/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  appstore_transition
+//
+//  Created by 周希 on 2018/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ appstore_transition/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  appstore_transition
+//
+//  Created by 周希 on 2018/1/15.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
